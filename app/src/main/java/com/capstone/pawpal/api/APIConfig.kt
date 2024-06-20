9c5230f4685ac19cfa8c47f03d0ac4aac775335e
Adindasfrs <shafira.sholihin@gmail.com>
2024-06-20 13:12:24 +0700

    yuk bisa

--- app/src/main/java/com/capstone/pawpal/api/APIConfig.kt
+++ app/src/main/java/com/capstone/pawpal/api/APIConfig.kt
@@ -6,22 +6,23 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class APIConfig {
-    companion object {
+object APIConfig {
         fun getApiService(): APIService {
             val loggingInterceptor =
-                if (BuildConfig.DEBUG)
+                if (BuildConfig.DEBUG){
                     HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-                else HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+    }else{
+
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
+        }
             val client = OkHttpClient.Builder()
                 .addInterceptor(loggingInterceptor)
                 .build()
             val retrofit = Retrofit.Builder()
-                .baseUrl(BuildConfig.API_URL)
+                .baseUrl("https://9a71-180-244-138-231.ngrok-free.app")
                 .addConverterFactory(GsonConverterFactory.create())
                 .client(client)
                 .build()
             return retrofit.create(APIService::class.java)
         }
     }
-}
