a3d088290848706c7423a58a5618ebacd90a86e2
Adindasfrs <shafira.sholihin@gmail.com>
2024-06-20 14:21:53 +0700

    yooo

--- app/src/main/java/com/capstone/pawpal/api/APIConfig.kt
+++ app/src/main/java/com/capstone/pawpal/api/APIConfig.kt
@@ -1,9 +1,7 @@
 package com.capstone.pawpal.api
 
-import com.capstone.pawpal.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
