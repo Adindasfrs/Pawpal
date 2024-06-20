2906cd2c174bca6ecac1e4ea7e9b867c58db5192
Adindasfrs <shafira.sholihin@gmail.com>
2024-06-20 14:19:22 +0700

    push

--- app/src/main/java/com/capstone/pawpal/api/APIConfig.kt
+++ app/src/main/java/com/capstone/pawpal/api/APIConfig.kt
@@ -1,28 +1,29 @@
 package com.capstone.pawpal.api
 
 import com.capstone.pawpal.BuildConfig
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object APIConfig {
-        fun getApiService(): APIService {
-            val loggingInterceptor =
-                if (BuildConfig.DEBUG){
-                    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-    }else{
-
-            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
-        }
-            val client = OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
+    fun getApiService(): APIService {
+        val authInterceptor = Interceptor { chain ->
+            val req = chain.request()
+            val requestHeaders = req.newBuilder()
+                .addHeader("Authorization", "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")
                 .build()
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://9a71-180-244-138-231.ngrok-free.app")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(client)
-                .build()
-            return retrofit.create(APIService::class.java)
+            chain.proceed(requestHeaders)
         }
+        val client = OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://9cb2-180-244-138-231.ngrok-free.app")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+        return retrofit.create(APIService::class.java)
+    }
     }
